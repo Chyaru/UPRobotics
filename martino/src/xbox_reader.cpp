fb43780b71757b3fbbc26a81bd9cc57b6d9d2a4c
Chyaru <82061637+Chyaru@users.noreply.github.com>
2023-06-16 11:49:15 -0600

    xbox

--- martino/src/xbox_reader.cpp
+++ martino/src/xbox_reader.cpp
@@ -5,8 +5,62 @@
 
 
 void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy){
-  /*  int r = joy->buttons[4];
-    if(r!=0){
+    std::vector<double> axis[8];
+    std::vector<bool> botons[12];
+    for(int i = 0; i < 8; i++) axis[i] = joy->axes[i];
+    for(int i = 0; i < 12; i++) botons[i]=joy->buttons[i]; 
+    /*
+        axis 0 joystick de arriba horizontal
+        axis 1 joystick de arriba vertical
+        axis 2 RT
+        axis 3 joystick de abajo horizontal
+        axis 4 joystick de abajo vertical
+        axis 5 LT
+        axis 6 flechita horizontal
+        axis 7 flechita vertical
+        boton 0 es A
+        boton 1 es B
+        boton 2 es X
+        boton 3 es Y
+        boton 4 es LB
+        boton 5 es RB
+        boton 6 es doble cuadrito
+        boton 7 es 3 lineas
+        boton 8 es xbox
+        boton 9 es presionar ruedita de arriba
+        boton 10 es presionar ruedita de abajo
+        boton 11 es captura
+    */
+    int r=-2;
+
+    if(axis[0]==-1.0) r = ;
+    else if(axis[0]==1.0) r = ;
+    else if(axis[1]==-1.0) r = ;
+    else if(axis[1]==1.0) r = ;
+    else if(axis[2]==-1.0) r = ;
+    else if(axis[3]==-1.0) r = ; 
+    else if(axis[3]==1.0) r = ;
+    else if(axis[4]==-1.0) r = ;
+    else if(axis[4]==1.0) r = ;
+    else if(axis[5]==-1.0) r = ;
+    else if(axis[6]==-1.0) r = ;
+    else if(axis[6]==1.0) r = ;
+    else if(axis[7]==-1.0) r = ;
+    else if(axis[7]==1.0) r = ;
+    else if(botons[0]) r = ;
+    else if(botons[1]) r = ;
+    else if(botons[2]) r = ;
+    else if(botons[3]) r = ;
+    else if(botons[4]) r = ;
+    else if(botons[5]) r = ;
+    else if(botons[6]) r = ;
+    else if(botons[7]) r = ;
+    else if(botons[8]) r = ;
+    else if(botons[9]) r = ;
+    else if(botons[10]) r = ;
+    else if(botons[11]) r = ;
+
+    if(r!=-2){
         std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("reader_client");
         rclcpp::Client<structures::srv::Movement>::SharedPtr client =
         node->create_client<structures::srv::Movement>("direcciones");
@@ -23,58 +77,7 @@ void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy){
         } else {
             RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service direcciones");
         }
-    }
-*/
-
-/*
-
-        rclcpp::Client<structures::srv::Movement>::SharedPtr client_;
-        client_ = rclcpp::create_client<structures::srv::Movement>("martino/server"); 
-        auto future = client_->async_send_request(request);   
-*/
-
-        /* auto responseReceived = future.wait_for(std::chrono::milliseconds(200));
-            if (responseReceived == std::future_status::ready) {
-            auto response = future.get();
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "succesful");
-            } else {
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "not succesful");
-            }
-        */
-   
-
-    std::vector<double> states(20);
-   for(int i = 0; i < 8; i++){
-        states[i]=joy->axes[i];
-        if(i!=2 && i!=5 && states[i]!=0.0)
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "button %d is %f", i, states[i]);
-    }
-    
-    /*
-        axis 0 joystick de arriba horizontal
-        axis 1 joystick de arriba vertical
-        axis 2 RT
-        axis 3 joystick de abajo horizontal
-        axis 4 joystick de abajo vertical
-        axis 5 LT
-        axis 6 flechita horizontal
-        axis 7 flechita vertical
-        
-
-        boton 0 es A
-        boton 1 es B
-        boton 2 es X
-        boton 3 es Y
-        boton 4 es LB
-        boton 5 es RB
-        boton 6 es doble cuadrito
-        boton 7 es 3 lineas
-        boton 8 es xbox
-        boton 9 es presionar ruedita de arriba
-        boton 10 es presionar ruedita de abajo
-        boton 11 es captura
-
-        */
+    }   
 
     
     return;
