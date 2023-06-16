2cf989f44d325de5107e629dc37c2d339b77e837
Chyaru <82061637+Chyaru@users.noreply.github.com>
2023-06-16 11:22:32 -0600

    Update xbox_reader.cpp

--- martino/src/xbox_reader.cpp
+++ martino/src/xbox_reader.cpp
@@ -1,12 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "structures/srv/movement.hpp"
-//#include<vector>
+#include<vector>
 
 
 void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy){
-  // int d1 = ... d17 = ...
-    int r = joy->buttons[4]; // define the r
+  /*  int r = joy->buttons[4];
     if(r!=0){
         std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("reader_client");
         rclcpp::Client<structures::srv::Movement>::SharedPtr client =
@@ -24,7 +23,8 @@ void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy){
         } else {
             RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service direcciones");
         }
-
+    }
+*/
 
 /*
 
@@ -41,34 +41,39 @@ void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy){
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "not succesful");
             }
         */
-    }
+   
 
-    
-  /* for(int i = 0; i < 15; i++){
-        states[i]=joy->buttons[i];
-        if(states[i]!=0.0)
+    std::vector<double> states(20);
+   for(int i = 0; i < 8; i++){
+        states[i]=joy->axes[i];
+        if(i!=2 && i!=5 && states[i]!=0.0)
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "button %d is %f", i, states[i]);
     }
-    */
+    
     /*
-        axis 0 es rueda de arriba horizontal
-        axis 1 es rueda de arriba vertical
-        axis 2 es rueda de abajo horizontal
-        axis 3 es rueda de abajo vertical
-        
+        axis 0 joystick de arriba horizontal
+        axis 1 joystick de arriba vertical
+        axis 2 RT
+        axis 3 joystick de abajo horizontal
+        axis 4 joystick de abajo vertical
+        axis 5 LT
+        axis 6 flechita horizontal
+        axis 7 flechita vertical
         
 
         boton 0 es A
         boton 1 es B
-        boton 3 es X
-        boton 4 es Y
-        boton 9 es RT
-        boton 8 es LT
-        boton 7 es RB
-        boton 6 es LB
-        boton 11 es start
-        boton 10 es back
-        boton 13 es el del centro
+        boton 2 es X
+        boton 3 es Y
+        boton 4 es LB
+        boton 5 es RB
+        boton 6 es doble cuadrito
+        boton 7 es 3 lineas
+        boton 8 es xbox
+        boton 9 es presionar ruedita de arriba
+        boton 10 es presionar ruedita de abajo
+        boton 11 es captura
+
         */
 
     
