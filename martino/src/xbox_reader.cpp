146fbe02348f9bd3dda8973c438ff6fe1bb61957
Chyaru <82061637+Chyaru@users.noreply.github.com>
2023-06-15 19:33:02 -0600

    xb

--- martino/src/xbox_reader.cpp
+++ martino/src/xbox_reader.cpp
@@ -1,43 +1,55 @@
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/joy.hpp"
-#include "structures/srv/movement.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/Joy.hpp>
+#include <service_package/srv/ServiceType.hpp>
 
-void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy)
+class JoystickReaderNode : public rclcpp::Node
 {
-    int direction = 0;
-    
-    // int d1 = , d2 ... d17 
-    // if(d1 && ...) direction = 1 ...
+public:
+  JoystickReaderNode() : Node("joystick_reader_node")
+  {
+    // Create subscriber
+    joy_sub_ = create_subscription<sensor_msgs::msg::Joy>(
+      "/joy", 10, std::bind(&JoystickReaderNode::joyCallback, this, std::placeholders::_1));
 
-    auto request = std::make_shared<structures::srv::Movement::Request>();
-    request->direction = direction;
+    // Create client
+    client_ = create_client<service_package::srv::ServiceType>("/service_name");
+  }
 
-    client_ = create_client<structures::srv::Movement>("client");
-    client_->async_send_request(request);
+private:
+  void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy)
+  {
+    // Process the received joystick message
+    // ...
+
+    // Create a request object
+    auto request = std::make_shared<service_package::srv::ServiceType::Request>();
+
+    // Populate the request with relevant data based on the received message
+    // ...
+
+    // Send the request to the service server
     auto future = client_->async_send_request(request);
 
-    /* auto responseReceived = future.wait_for(std::chrono::milliseconds(200));
+    // Handle the response when it arrives
+    auto responseReceived = future.wait_for(std::chrono::seconds(5));
 
     if (responseReceived == std::future_status::ready) {
       auto response = future.get();
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "succesful");
+      // Process the response received from the service server
     } else {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "not succesful");
+      // Handle case when no response is received within the specified time
     }
-    */
-}
+  }
+
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
+  rclcpp::Client<service_package::srv::ServiceType>::SharedPtr client_;
+};
 
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("joystick_reader");
-
-  auto joy_sub = node->create_subscription<sensor_msgs::msg::Joy>(
-    "/joy", 10, joyCallback);
-
+  auto node = std::make_shared<JoystickReaderNode>();
   rclcpp::spin(node);
-
   rclcpp::shutdown();
-
   return 0;
 }
